dd6148c195b4762fbe7044dc5cfca638bdf43108
smhrd <smhrd>
2021-12-18 17:15:28 +0900

    ugly songmi

--- app/src/main/java/com/csm/defendo/join.java
+++ app/src/main/java/com/csm/defendo/join.java
@@ -10,6 +10,7 @@ public class join extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+        //songmi
 
     }
 }
